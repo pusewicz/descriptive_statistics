fda48c2d452d5ae2a13f11c4c9b6cf4996aac64d
Piotr Usewicz <piotr@layer22.com>
2017-02-17 17:59:32 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule DescriptiveStatistics.Mixfile do
 
   def project do
     [ app: :descriptive_statistics,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "> 0.13.2",
       deps: [],
       package: [
