c25545d53c40d0f0e141c676986b63dd70275882
Piotr Usewicz <piotr@layer22.com>
2017-02-17 17:59:13 +0100

    Cleanup mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule DescriptiveStatistics.Mixfile do
     [ app: :descriptive_statistics,
       version: "0.0.1",
       elixir: "> 0.13.2",
-      deps: deps,
+      deps: [],
       package: [
         contributors: ["Piotr Usewicz", "Victor Martinez"],
         licenses: ["MIT"],
@@ -15,17 +15,7 @@ defmodule DescriptiveStatistics.Mixfile do
     ]
   end
 
-  # Configuration for the OTP application
   def application do
-    []
-  end
-
-  # Returns the list of dependencies in the format:
-  # { :foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1" }
-  #
-  # To specify particular versions, regardless of the tag, do:
-  # { :barbat, "~> 0.1", github: "elixir-lang/barbat.git" }
-  defp deps do
     []
   end
 end
